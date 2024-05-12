dc594e8989f633130f4d758f2e19b488ff62ba5c
Yudha Prawira <iniakureal@gmail.com>
2024-05-12 20:21:02 +0700

    add: tailwinds color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-5xl font-light">If a squirrel</h1>
+      <h1 className="text-5xl font-light">NAYAKANTA</h1>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors : {
+      "yellow" : "#FFB700",
+      "green" : "#079269",
+      "pink" : "#E47BE5",
+      "orange" : "#FF7917",
+      "purple" : "#3131BC",
+      "blue" : "#0D82F9",
+      "silver-blue" : "#9FB8F4",
+      "black" : "#0B0A0A",
+      "gray" : "#18191E",
+      "light-gray" : "#222528"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
